8afed21c019e0f06762998fe8450e517e4cf594c
Harshitha <harshithadarshan95@gmail.com>
2023-02-24 11:24:30 +0530

    Autopush

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 module "mongodb" {
   source               = "./vendor/modules/mongodb"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                  = var.ENV
+  DOCDB_PORT           = var.DOCDB_PORT
+  DOCDB_INSTANCE_CLASS = var.DOCDB_INSTANCE_CLASS
+  DOCDB_INSTANCE_COUNT = var.DOCDB_INSTANCE_COUNT
+
 }
 
 module "redis" {
@@ -11,4 +15,13 @@ module "redis" {
 module "mysql" {
   source               = "./vendor/modules/mysql"     # Terrafile is going to download the code from the respective branch of the repo and keeps it local
   ENV                  = var.ENV
+  RDS_MYSQL_PORT       = var.RDS_MYSQL_PORT
+  RDS_MYSQL_STORAGE    = var.RDS_MYSQL_STORAGE
+  RDS_MYSQL_ENGINE_VERSION = var.RDS_MYSQL_ENGINE_VERSION
+  RDS_INSTANCE_TYPE    = var.RDS_INSTANCE_TYPE
+
+}
+
+output "redis" {
+    value = module.redis.redis
 }
