cb1402909e3825a5ab4e21ceb4061c1309d5255a
Harshitha <harshithadarshan95@gmail.com>
2023-02-24 11:25:12 +0530

    Autopush

--- vars.tf
+++ vars.tf
@@ -1,1 +1,12 @@
 variable "ENV" {}
+
+# RDS
+variable "RDS_MYSQL_PORT" {}
+variable "RDS_MYSQL_STORAGE" {}
+variable "RDS_MYSQL_ENGINE_VERSION" {}
+variable "RDS_INSTANCE_TYPE" {}
+
+# DocDB 
+variable "DOCDB_PORT" {}
+variable "DOCDB_INSTANCE_CLASS" {}
+variable "DOCDB_INSTANCE_COUNT" {}
